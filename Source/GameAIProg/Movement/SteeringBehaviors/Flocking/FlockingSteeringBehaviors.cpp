096c2c8f2fccfc0a91f256c973eb8f8f49f7ceef
MireauLijbaert <mireau.lijbaert@student.howest.be>
2026-03-01 14:24:33 +0100

    Added FlockingSteeringBehaviors

--- Source/GameAIProg/Movement/SteeringBehaviors/Flocking/FlockingSteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Flocking/FlockingSteeringBehaviors.cpp
@@ -8,11 +8,35 @@
 //COHESION (FLOCKING)
 SteeringOutput Cohesion::CalculateSteering(float deltaT, ASteeringAgent& pAgent)
 {
-	return SteeringOutput{};
+	Target.Position = pFlock->GetAverageNeighborPos();
+	
+	return Seek::CalculateSteering(deltaT, pAgent);
 }
 
 //*********************
 //SEPARATION (FLOCKING)
+SteeringOutput Separation::CalculateSteering(float deltaT, ASteeringAgent& pAgent)
+{
+	SteeringOutput steering{};
+	
+	if (pFlock->GetNrOfNeighbors() == 0) return steering;
+	
+	for (int i = 0; i < pFlock->GetNrOfNeighbors(); ++i)
+	{
+		FVector Direction = pAgent.GetActorLocation()- pFlock->GetNeighbors()[i]->GetActorLocation();
+		FVector2D Direction2D = FVector2D{ Direction.X, Direction.Y };
+		steering.LinearVelocity += Direction2D.GetSafeNormal() * (1.f/Direction2D.Length());
+	}
+	steering.LinearVelocity = steering.LinearVelocity.GetSafeNormal() * pAgent.GetMaxLinearSpeed();
+	return steering;
+}
+
 
 //*************************
 //VELOCITY MATCH (FLOCKING)
+SteeringOutput Alignment::CalculateSteering(float deltaT, ASteeringAgent& pAgent)
+{
+	SteeringOutput steering{};
+	steering.LinearVelocity = pFlock->GetAverageNeighborVelocity().GetSafeNormal() * pAgent.GetMaxLinearSpeed();
+	return steering; 
+}
--- Source/GameAIProg/Movement/SteeringBehaviors/Flocking/FlockingSteeringBehaviors.h
+++ Source/GameAIProg/Movement/SteeringBehaviors/Flocking/FlockingSteeringBehaviors.h
@@ -9,7 +9,7 @@ class Cohesion final : public Seek
 public:
 	Cohesion(Flock* const pFlock) :pFlock(pFlock) {};
 
-	//Cohesion Behavior
+	// Cohesion Behavior
 	SteeringOutput CalculateSteering(float deltaT, ASteeringAgent& pAgent) override;
 
 private:
@@ -18,6 +18,28 @@ private:
 
 //SEPARATION - FLOCKING
 //*********************
+class Separation final : public ISteeringBehavior
+{
+public:
+	Separation(Flock* const pFlock) :pFlock(pFlock) {};
+	
+	// Separation Behavior
+	SteeringOutput CalculateSteering(float deltaT, ASteeringAgent& pAgent) override;
+	
+private:
+	Flock* pFlock = nullptr;
+};
 
 //VELOCITY MATCH - FLOCKING
 //************************
+class Alignment final : public ISteeringBehavior
+{
+public:
+	Alignment(Flock* const pFlock) :pFlock(pFlock) {};
+	
+	// Alignment Behavior
+	SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) override;
+	
+private:
+	Flock* pFlock = nullptr;
+};
