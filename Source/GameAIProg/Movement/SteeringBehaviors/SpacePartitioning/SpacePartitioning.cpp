6a88274c7a51e831b8759206ac04a5d0026b12e4
MireauLijbaert <mireau.lijbaert@student.howest.be>
2026-03-08 19:33:28 +0100

    Wrote the SpacePartitioning functions and uncommented the spacepartitioning definition and commented code in flock

--- Source/GameAIProg/Movement/SteeringBehaviors/SpacePartitioning/SpacePartitioning.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/SpacePartitioning/SpacePartitioning.cpp
@@ -43,23 +43,71 @@ CellSpace::CellSpace(UWorld* pWorld, float Width, float Height, int Rows, int Co
 	CellHeight = Height / Rows;
 
 	// TODO create the cells
+	for (int row = 0; row < Rows; ++row)
+	{
+		for (int col = 0; col < Cols; ++col)
+		{
+			Cells.emplace_back(Cell(col * CellWidth, row * CellHeight, CellWidth, CellHeight));
+		}
+	}
 }
 
 void CellSpace::AddAgent(ASteeringAgent& Agent)
 {
 	// TODO Add the agent to the correct cell
+	int cellIndex = PositionToIndex(Agent.GetPosition());
+	Cells[cellIndex].Agents.emplace_back(&Agent);
+	
 }
 
 void CellSpace::UpdateAgentCell(ASteeringAgent& Agent, const FVector2D& OldPos)
 {
 	//TODO Check if the agent needs to be moved to another cell.
 	//TODO Use the calculated index for oldPos and currentPos for this
+	int oldIndex = PositionToIndex(OldPos);
+	int newIndex = PositionToIndex(Agent.GetPosition());
+
+	// If the agent moved to another cell
+	if (oldIndex != newIndex)
+	{
+		// Remove from old cell
+		Cells[oldIndex].Agents.remove(&Agent);
+
+		// Add to new cell
+		Cells[newIndex].Agents.push_back(&Agent);
+	}
 }
 
 void CellSpace::RegisterNeighbors(ASteeringAgent& Agent, float QueryRadius)
 {
 	// TODO Register the neighbors for the provided agent
 	// TODO Only check the cells that are within the radius of the neighborhood
+	NrOfNeighbors = 0;
+
+	FVector2D agentPos = Agent.GetPosition();
+
+	FRect neighborRect;
+	neighborRect.Min = FVector2D(agentPos.X - QueryRadius, agentPos.Y - QueryRadius);
+	neighborRect.Max = FVector2D(agentPos.X + QueryRadius, agentPos.Y + QueryRadius);
+
+	for (Cell& cell : Cells)
+	{
+		if (DoRectsOverlap(cell.BoundingBox, neighborRect))
+		{
+			for (ASteeringAgent* otherAgent : cell.Agents)
+			{
+				if (otherAgent == &Agent) continue;
+
+				float distance = FVector2D::Distance(otherAgent->GetPosition(), agentPos);
+
+				if (distance <= QueryRadius)
+				{
+					Neighbors[NrOfNeighbors] = otherAgent;
+					++NrOfNeighbors;
+				}
+			}
+		}
+	}
 }
 
 void CellSpace::EmptyCells()
@@ -71,12 +119,40 @@ void CellSpace::EmptyCells()
 void CellSpace::RenderCells() const
 {
 	// TODO Render the cells with the number of agents inside of it
+	for (const Cell& cell : Cells)
+	{
+		const std::vector<FVector2D> points = cell.GetRectPoints();
+
+		for (size_t i = 0; i < points.size(); ++i)
+		{
+			FVector start(points[i].X, points[i].Y, 5.f);
+			FVector end(
+				points[(i + 1) % points.size()].X,
+				points[(i + 1) % points.size()].Y,
+				5.f);
+
+			DrawDebugLine(pWorld, start, end, FColor::White, false, -1.f, 0, 1.5f);
+		}
+
+		FVector2D textPos2D = points[1]; // top-left
+		FVector textPos(textPos2D.X, textPos2D.Y, 20.f);
+
+		DrawDebugString(
+			pWorld,
+			textPos,
+			FString::FromInt(cell.Agents.size()),
+			nullptr,
+			FColor::White,
+			0.f);
+	}
 }
 
 int CellSpace::PositionToIndex(FVector2D const & Pos) const
 {
 	// TODO Calculate the index of the cell based on the position
-	return 0;
+	int rowIndex = std::min(int(Pos.Y / SpaceHeight * NrOfRows), NrOfRows-1);
+	int colIndex = std::min(int(Pos.X / SpaceWidth * NrOfCols), NrOfCols-1);
+	return rowIndex * NrOfCols + colIndex;
 }
 
 bool CellSpace::DoRectsOverlap(FRect const & RectA, FRect const & RectB)
