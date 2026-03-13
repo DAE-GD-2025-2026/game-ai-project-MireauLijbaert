3ce303cbbe0a8faff4fc3a98973ea1879cde9f34
MireauLijbaert <mireau.lijbaert@student.howest.be>
2026-03-13 15:46:20 +0100

    Added the todo logic required, also Changed the Renderer to have a world as this was initialized without one which crashed because renderer tried drawing to a nullptr, Create path in the beginplay once as we can't change the playergraphEditor flag for if it has updated

--- Source/GameAIProg/GraphTheory/Level_GraphTheory.cpp
+++ Source/GameAIProg/GraphTheory/Level_GraphTheory.cpp
@@ -34,6 +34,7 @@ void ALevel_GraphTheory::BeginPlay()
 		UE_LOG(LogTemp, Error, TEXT("Unable to get PlayerController from LocalPlayer or GraphEditorClass is null"))
 		return;
 	}
+
 	
 	// Make the view orthogonal for less perspective issues
 	if (AGameAISpectator* Player = Cast<AGameAISpectator>(PlayerController->GetPawnOrSpectator()); Player)
@@ -41,12 +42,35 @@ void ALevel_GraphTheory::BeginPlay()
 		Player->SetCameraProjection(ECameraProjectionMode::Orthographic);
 	}
 	
+	// Set the world for the renderer
+	Renderer = GameAI::GraphRenderer(GetWorld());
+	
 	// TODO Make the graph and a couple connected nodes here...
+	// Make nodes
+	Graph.AddNode(NodeFactory.CreateNode(FVector2D{100.f, 100.f}));
+	Graph.AddNode(NodeFactory.CreateNode(FVector2D{300.f, 100.f}));
+	Graph.AddNode(NodeFactory.CreateNode(FVector2D{200.f, 250.f}));
+
+	// Connect nodes
+	Graph.AddConnection(0, 1);
+	Graph.AddConnection(1, 2);
+	Graph.AddConnection(2, 0);
+	
+	
 	
 	// Spawn the Agent
 	Agent = GetWorld()->SpawnActor<ASteeringAgent>(SteeringAgentClass, 
 	FVector{0,0,90}, FRotator::ZeroRotator);
 	Agent->SetSteeringBehavior(&PathFollow);
+	
+	// Create path for the first time
+	EulerianPath eulerianPath(&Graph);
+	Eulerianity eulerianity = eulerianPath.IsEulerian();
+	auto trail = eulerianPath.FindPath(eulerianity);
+	UpdateAgentPath(trail);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Graph node count: %i"), Graph.GetNodeCount());
+	UE_LOG(LogTemp, Warning, TEXT("Active node count: %i"), Graph.GetActiveNodes().size());
 }
 
 void ALevel_GraphTheory::BeginDestroy()
@@ -100,8 +124,18 @@ void ALevel_GraphTheory::Tick(float DeltaTime)
 	Renderer.RenderGraph(Graph);
 	
 	// TODO Check if the graph has updated
-	// TODO if so, run the EulerianPath algorithm
-	// TODO if a path is found, have the agent follow it
+	if (PlayerGraphEditor && PlayerGraphEditor->HasGraphUpdated())
+	{
+		// TODO if so, run the EulerianPath algorithm
+		EulerianPath EulerianPath(&Graph);
+		Eulerianity eulerianity = EulerianPath.IsEulerian();
+		auto trail = EulerianPath.FindPath(eulerianity);
+		// TODO if a path is found, have the agent follow it
+		// If no path is found we want the agent to not move so we pass the trail if it is empty too
+		UpdateAgentPath(trail);
+	}
+	
+	
 }
 
 void ALevel_GraphTheory::UpdateAgentPath(std::vector<Node*> const& Trail)
@@ -109,6 +143,10 @@ void ALevel_GraphTheory::UpdateAgentPath(std::vector<Node*> const& Trail)
 	std::vector<FVector2D> path{};
 	
 	// TODO convert Node vector to positions vector
+	for (auto const& node : Trail)
+	{
+		path.emplace_back(node->GetPosition());
+	}
 
 	PathFollow.SetPath(path);
 	if (path.size() > 0)
